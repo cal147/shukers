871828f2ca2753e9ef30603fb1900a03651132d2
Chris Laine <laine.chris1@gmail.com>
2017-07-16 22:35:43 +0100

    Test for branch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "../node_modules/semantic-ui/dist/semantic.min.css";
+
 
 //test for github
 
 //TODO complete the project
 
 //GitHub push test.
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
