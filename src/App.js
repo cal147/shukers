b47d3fe7d5227cdf2280800cffd6d54d0b0dd7ec
Carl Leatherbarrow <carl@clps.uk>
2017-07-17 08:31:56 +0100

    Made some changes in the index.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import "./App.css";
-import {BrowserRouter, Link, Route} from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 
 import {Dropdown, Input, Menu} from "semantic-ui-react";
 
@@ -21,24 +21,25 @@ export default class App extends Component {
             <div>
                 <BrowserRouter>
                     <div>
-                        <Menu inverted color={'red'} stackable>
-                            <Menu.Item name='home' active={activeItem === 'home' } onClick={this.handleItemClick}
-                                       as={Link} to='/'/>
-                            <Menu.Item name='about' active={activeItem === 'about' } onClick={this.handleItemClick}
-                                       as={Link} to='/about'/>
-                            <Dropdown item text={'Products'}>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item>Pork</Dropdown.Item>
-                                    <Dropdown.Item>Beef</Dropdown.Item>
-                                    <Dropdown.Item>Game</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Menu.Menu position='right'>
-                                <Menu.Item>
-                                    <Input icon='search' placeholder='Search...'/>
-                                </Menu.Item>
-                            </Menu.Menu>
-                        </Menu>
+                        <!-- This code should be in a layout file leaving this class just to handle routing. Change the div to Switch. In layout just put {this.props.children} where you want to render the page -->
+                        {/*<Menu inverted color={'red'} stackable>*/}
+                            {/*<Menu.Item name='home' active={activeItem === 'home' } onClick={this.handleItemClick}*/}
+                                       {/*as={Link} to='/'/>*/}
+                            {/*<Menu.Item name='about' active={activeItem === 'about' } onClick={this.handleItemClick}*/}
+                                       {/*as={Link} to='/about'/>*/}
+                            {/*<Dropdown item text={'Products'}>*/}
+                                {/*<Dropdown.Menu>*/}
+                                    {/*<Dropdown.Item>Pork</Dropdown.Item>*/}
+                                    {/*<Dropdown.Item>Beef</Dropdown.Item>*/}
+                                    {/*<Dropdown.Item>Game</Dropdown.Item>*/}
+                                {/*</Dropdown.Menu>*/}
+                            {/*</Dropdown>*/}
+                            {/*<Menu.Menu position='right'>*/}
+                                {/*<Menu.Item>*/}
+                                    {/*<Input icon='search' placeholder='Search...'/>*/}
+                                {/*</Menu.Item>*/}
+                            {/*</Menu.Menu>*/}
+                        {/*</Menu>*/}
 
                         <hr/>
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import "../node_modules/semantic-ui/dist/semantic.min.css";
-
+import 'semantic-ui-css/semantic.min.css';
 
 
 
