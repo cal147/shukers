c1199916c271998bca40240aadfc5c3fb59e432d
Chris Laine <laine.chris1@gmail.com>
2017-07-19 18:36:35 +0100

    upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import home from "./site/Pages/Home/Home";
 import About from "./site/Pages/About/About";
 import SiteLayout from './site/Layout/layout'
 import MainPage from './admin/index';
+import Header from "./site/Layout/Header/Header"
+import Footer from "./site/Layout/Footer/Footer"
+import "./site/Layout/Layout.css"
 
 
 //This component will do the routing for the public side of the app.
@@ -15,13 +18,16 @@ const Site = ()=>{
                             //******************************************************
   return(
       <HashRouter>
-        <div>
+        <div className="publicPageLayout" >
+            <Header />
             <SiteLayout/>
             <hr/>
             <Switch>
                 <Route exact path="/" component={home}/>
                 <Route path="/about" component={About}/>
+
             </Switch>
+            <Footer />
         </div>
     </HashRouter>
   );
---
+++ src/site/Layout/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export default class Footer extends Component{
+
+    render(){
+        return(
+            <div>
+                <hr/>
+                    <div>
+                        <h4>Copyright CLPS{'\u00A9'}</h4>
+
+                    </div>
+            </div>
+        )
+    }
+}
