2569c0a033b5ecbd19612124b49677f00716e57a
Chris Laine <laine.chris1@gmail.com>
2017-07-21 16:09:27 +0100

    Slider working

--- src/site/Pages/Home/Home.jsx
+++ src/site/Pages/Home/Home.jsx
@@ -1,21 +1,43 @@
 import React, {Component} from "react";
+import { imgResource } from '../../../shared/urls'
+import Slider from 'react-slick'
+
 
 export default class home extends Component {
 
     render() {
+
+        var settings = {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplayspeed: 10000,
+            speed: 1000,
+            fade: true,
+            pauseOnHover: true,
+            adaptHeight: true,
+            accessibility: true,
+            swipeToSlide: true,
+            arrows: false,
+        };
+
         return (
-            <div>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
-                <h2>Home Page</h2>
+            <div >
+                <h1>ImageSlider goes here!!</h1>
+<div className="siteSlider">
+                <Slider {...settings}>
+                    <img src={imgResource+"Logo.png"}/>
+                    <img src={imgResource+"Cow.png"}/>
+                    <img src={imgResource+"Pig.png"}/>
+                </Slider>
+            </div>
+
+
+
+
+
+
             </div>
         )
     }
