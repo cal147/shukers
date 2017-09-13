0f8941a0f80e6d30138885e530176a180b746dc4
Carl Leatherbarrow <carl@clps.uk>
2017-09-13 16:26:57 +0100

    Picture is now uploading

--- public/Scripts/admin/Controllers/productsController.php
+++ public/Scripts/admin/Controllers/productsController.php
@@ -115,6 +115,7 @@ if(session_status() === PHP_SESSION_ACTIVE) {
             $b64dec = base64_decode($newFileData);
             fwrite($theFile, $b64dec);
             fclose($theFile);
+
         }
     }
 }
