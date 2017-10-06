2fb0754b5282e485cac5d817aa2f3f792a05bf64
cal147 <laine.chris1@gmail.com>
2017-10-06 10:53:48 +0100

    forgotten page looking nice

--- src/site/Pages/Login/ForgottenPassword.jsx
+++ src/site/Pages/Login/ForgottenPassword.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Link} from 'react-router-dom';
-import {Button, Form, Segment} from 'semantic-ui-react';
+import {Button, Segment} from 'semantic-ui-react';
 
 export default class login extends Component {
 
@@ -9,10 +9,12 @@ export default class login extends Component {
         return (
             <div>
                 <Segment inverted color="red">
-                    <form action="/resetpass.php" method="GET">
+                    <form action="/resetpass.php" method="GET" className="ui form">
                         Email associated with the account<br/>
-                        <input type="email" name="email"/><br/>
-                        <input type="submit" value="submit" />
+                        <div className="required four wide field">
+                            <input type="email" name="email" className="ui input"/><br/>
+                            <input type="submit" value="submit" className="ui black button"/>
+                        </div>
                     </form>
                 </Segment>
                 <Button as={Link} to="/signUp" negative>To create an account click here</Button>
