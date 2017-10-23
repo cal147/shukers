62a3f82336875ad8bc00ddf6bfd92f92d1cf503d
Carl1983 <carl@clps.uk>
2017-10-23 12:23:56 +0100

    Update dataBaseConn.php

--- public/Scripts/Shared/dataBaseConn.php
+++ public/Scripts/Shared/dataBaseConn.php
@@ -2,10 +2,10 @@
 
  class Database{
      //Constant mysql details ensures they cant be changed later in the program.
-    const SERVER = 'db702337542.db.1and1.com';
-    const USERNAME = 'dbo702337542';
-    const PASSWORD = 'Shuker.2';
-    const DATABASE = 'db702337542';
+    const SERVER = '127.0.0.1';
+    const USERNAME = 'admin';
+    const PASSWORD = '123456';
+    const DATABASE = 'myDb';
 
     private $conn = null;
 
